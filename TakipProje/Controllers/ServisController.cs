44b648f0ca3a638a9ebac69d7d54f69a9f7696f6
Ahmet <ahmetbayrak859@gmail.com>
2021-07-07 15:25:35 +0300

    Layout Ayarlandı.

---
+++ TakipProje/Controllers/ServisController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using TakipProje.Models;
+
+namespace TakipProje.Controllers
+{
+    public class ServisController : Controller
+    {
+        private takipDbEntities db = new takipDbEntities();
+
+        // GET: Servis
+        public ActionResult Index()
+        {
+            return View(db.Servis.ToList());
+        }
+
+        // GET: Servis/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Servis servis = db.Servis.Find(id);
+            if (servis == null)
+            {
+                return HttpNotFound();
+            }
+            return View(servis);
+        }
+
+        // GET: Servis/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Servis/Create
+        // Aşırı gönderim saldırılarından korunmak için bağlamak istediğiniz belirli özellikleri etkinleştirin. 
+        // Daha fazla bilgi için bkz. https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,İsAdi,BakimPeriyodu,FirmaAdi,SabitTelefon,DestekPersonelAdSoyad,Gsm,Mail,BaslangicTarihi,BitisTarihi,BakimveAciklamalar")] Servis servis)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Servis.Add(servis);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(servis);
+        }
+
+        // GET: Servis/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Servis servis = db.Servis.Find(id);
+            if (servis == null)
+            {
+                return HttpNotFound();
+            }
+            return View(servis);
+        }
+
+        // POST: Servis/Edit/5
+        // Aşırı gönderim saldırılarından korunmak için bağlamak istediğiniz belirli özellikleri etkinleştirin. 
+        // Daha fazla bilgi için bkz. https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,İsAdi,BakimPeriyodu,FirmaAdi,SabitTelefon,DestekPersonelAdSoyad,Gsm,Mail,BaslangicTarihi,BitisTarihi,BakimveAciklamalar")] Servis servis)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(servis).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(servis);
+        }
+
+        // GET: Servis/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Servis servis = db.Servis.Find(id);
+            if (servis == null)
+            {
+                return HttpNotFound();
+            }
+            return View(servis);
+        }
+
+        // POST: Servis/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Servis servis = db.Servis.Find(id);
+            db.Servis.Remove(servis);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
