816f7c3b2bc6d1fa0917997d2e9a258563b1bb72
FerhatYondemli <yondemliferhat@gmail.com>
2021-07-07 23:45:08 +0300

    Lisans Index,Create,Edit,Details Tasarım Düzeltmeleri Yapıldı.

--- TakipProje/Controllers/LisansController.cs
+++ TakipProje/Controllers/LisansController.cs
@@ -84,7 +84,7 @@ namespace TakipProje.Controllers
             {
                 db.Entry(lisans).State = EntityState.Modified;
                 db.SaveChanges();
-                return RedirectToAction("Index");
+                return RedirectToAction("Edit");
             }
             return View(lisans);
         }
