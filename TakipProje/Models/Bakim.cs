92b9826003d043e1874476af3eec1fd996c1bae2
Ahmet <ahmetbayrak859@gmail.com>
2021-07-08 11:27:33 +0300

    Db nullable ayarları.

--- TakipProje/Models/Bakim.cs
+++ TakipProje/Models/Bakim.cs
@@ -16,7 +16,7 @@ namespace TakipProje.Models
     {
         public int ID { get; set; }
         public string BakimAdi { get; set; }
-        public Nullable<System.DateTime> BakimTarihi { get; set; }
+        public System.DateTime BakimTarihi { get; set; }
         public string BakimYapanPersonel { get; set; }
         public string Periyot { get; set; }
     }
