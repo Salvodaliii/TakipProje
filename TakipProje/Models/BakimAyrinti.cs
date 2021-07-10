05ef19ad70f3a4a7b8f893ad4c6e22d9c9c69905
Ahmet <ahmetbayrak859@gmail.com>
2021-07-10 18:11:59 +0300

    Eklemeler.

---
+++ TakipProje/Models/BakimAyrinti.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TakipProje.Models
+{
+    public class BakimAyrinti
+    {
+        public IEnumerable<Bakim> bakim { get; set; }
+        public IEnumerable<BakimAciklama> bakimNot { get; set; }
+    }
+}
