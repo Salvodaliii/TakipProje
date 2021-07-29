2edfd6496addc387acb3e3f902cc171d688f8260
FerhatYondemli <yondemliferhat@gmail.com>
2021-07-29 23:15:58 +0300

    Otomatik Mail Gönderme Sistemine Ait Geliştirme Ve Düzeltmeler Yapıldı. # Artık süresi <=10 olan tüm lisanslar TEK bir mail içerisinde kalan günleriyle ve tarihleriyle birlikte ilgili kişiye bildiriliyor.

--- TakipProje/Models/OtomatikMail/EmailJob.cs
+++ TakipProje/Models/OtomatikMail/EmailJob.cs
@@ -13,28 +13,104 @@ namespace TakipProje.Models.OtomatikMail
     {
         takipDbEntities db = new takipDbEntities();
 
+
         public void Execute(IJobExecutionContext context)
-        {
+        {        
+
+            Lisans lisans = new Lisans();
 
             DateTime bugunTarihi = DateTime.UtcNow;
             var lisansTablosu = db.Lisans.ToList();
 
+            int kayitsayisi = db.Lisans.Count(); // lisans tablosunda kaç kayıt var ?
+
+            string[] adkayit = new string[kayitsayisi];
+            DateTime[] tarihkayit = new DateTime[kayitsayisi];
+            int[] kalangunkayit = new int[kayitsayisi];
+
+            string ad = null;
+            DateTime tarih = lisans.AlimTarihi; //başlangıç değeri olması amacıyla eklendi , bir işlevi yok.
+
+            int kalangun = 0;
+            TimeSpan dif;
+
+
+
+            string ad2 = null;
+            DateTime tarih2 = lisans.AlimTarihi;//başlangıç değeri olması amacıyla eklendi , bir işlevi yok.
+
+
+            int[] fordegerleri = new int[60];
+
+
+            //indisler için atamalar ;
+            int j = 0; //adların sayısı
+            int r = 0; //tarihlerin sayısı
+            int f = 0; //kalangünlerin sayısı
+            int w = 0; //<=10 günü kalan lisanslar
+
+            bool mailatilacak = false; //en az bir lisans <=10 ise bu değer 1 olacak (true)
+
             foreach (var m in lisansTablosu)
             {
-                var tarih = m.BitisTarihi;
-                TimeSpan dif = bugunTarihi - tarih;
-                int a = Convert.ToInt32(dif.TotalDays);
-                a = a * (-1);
+                ad = m.ProgramAdi;
+                tarih = m.BitisTarihi;
+                
 
+                dif = bugunTarihi - tarih;
+                kalangun = Convert.ToInt32(dif.TotalDays);
+                kalangun *= (-1);
 
-
-
-                if (a <= 10)
+                if(kalangun <= 10)
                 {
-                    using (var message = new MailMessage("lisansBitisTarihi@outlook.com", "yaz1100@outlook.com"))
+                    w++; // kaç tane <=10 kayıt var ?
+
+                    tarihkayit[r] = tarih;
+                    r++;
+
+                    adkayit[j] = ad;
+                    j++;
+
+                    kalangunkayit[f] = kalangun;
+                    f++;
+
+                    mailatilacak = true;
+                }
+
+                
+
+
+                //for (int i = 0; i < 60; i++)
+                //{
+
+                //    if (adkayit[i] != ad) //BURAYA HERTÜRLÜ GİRİYOR, NEDEN ?
+                //    {
+                //        fordegerleri[i] = i;
+
+                //        adkayit[i] = ad;
+                //    }
+                //    else
+                //    {
+                //        i++;
+                //    }
+
+                //}
+            }
+
+
+
+                if (mailatilacak == true)
+                {
+                    using (var message = new MailMessage("lisansBitisTarihi@outlook.com", "takipproje@outlook.com"))
                     {
-                        message.Subject = "Lisans Süresi Hatırlatıcısı";
-                        message.Body = m.ProgramAdi + "  Adlı Lisans " + m.BitisTarihi.ToString("dd-MM-yyyy") + "  tarihinde bitiyor";
+
+                    message.Subject = w + " Adet Lisansın Süresi Bitmek Üzere!!!!!!";
+
+                    for (int k = 0; k < w; k++)
+                    {
+                        message.Body += "# " + adkayit[k] +" Adlı Lisansın Bitmesine " + kalangunkayit[k] + " Gün Kaldı. " + " Lisans Bitiş Tarihi : " + tarihkayit[k].ToString("dd-MM-yyyy") + " \n \r";
+                        }
+
                         using (SmtpClient client = new SmtpClient
                         {
                             EnableSsl = true,
@@ -42,40 +118,14 @@ namespace TakipProje.Models.OtomatikMail
                             Port = 587,
                             Credentials = new NetworkCredential("lisansBitisTarihi@outlook.com", "BitisTarihi00")
                         })
+
                         {
                             client.Send(message);
                         }
+
                     }
                 }
 
-                //a = a * (-1);
-                //if (a <= 0)
-                //{
-                //    using (var message = new MailMessage("lisansBitisTarihi@outlook.com", "yaz1100@outlook.com"))
-                //    {
-                //        message.Subject = "Uyarı! Lisans Süresi Bitti!";
-                //        message.Body = m.ProgramAdi + "  Adlı Lisans " + m.BitisTarihi.ToString("dd-MM-yyyy") + "  tarihinde bitmiştir.";
-                //        using (SmtpClient client = new SmtpClient
-                //        {
-                //            EnableSsl = true,
-                //            Host = "smtp-mail.outlook.com",
-                //            Port = 587,
-                //            Credentials = new NetworkCredential("lisansBitisTarihi@outlook.com", "BitisTarihi00")
-                //        })
-                //        {
-                //            client.Send(message);
-                //        }
-                //    }
-                //}
-
-
-
-
-
-                //unutma
-                //ad ve bitiş tarihi değerleri için bir for listesi oluşturulacak ve bu liste yine bir for ile indise göre göre çağırılacak ve top atacak.
-
-            }
 
         }
 
